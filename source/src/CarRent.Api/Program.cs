640fcf4f1adf407b0f928e2b8e0ad819138e562f
Cédric Menzi <cedric.menzi@buhlergroup.com>
2025-06-30 19:54:50 +0200

    add roslnyator

--- source/src/CarRent.Api/Program.cs
+++ source/src/CarRent.Api/Program.cs
@@ -8,7 +8,7 @@ namespace CarRent.Api
 
     using Microsoft.EntityFrameworkCore;
 
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
